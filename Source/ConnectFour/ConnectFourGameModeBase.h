716a35d05f24372e1103616b3e0de89354593565
Justin van Gemen <20277@ma-web.nl>
2017-05-03 20:36:28 +0200

    Initial Commit

---
+++ Source/ConnectFour/ConnectFourGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ConnectFourGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONNECTFOUR_API AConnectFourGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
